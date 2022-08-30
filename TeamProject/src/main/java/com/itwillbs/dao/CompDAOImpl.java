aefcb735ec037496ea31daba0334bc7d2974fe3b
k_nar <k_nar@DESKTOP-FTMSIG5>
2022-08-30 23:48:03 +0900

    220830 업체페이지 인서트완성(페이지 두개에서 하나로, 옵션두개 한번에 들어가게 바꿈)

--- TeamProject/src/main/java/com/itwillbs/dao/CompDAOImpl.java
+++ TeamProject/src/main/java/com/itwillbs/dao/CompDAOImpl.java
@@ -1,5 +1,7 @@
 package com.itwillbs.dao;
 
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -11,23 +13,24 @@ import com.itwillbs.domain.ProdStockDTO;
 
 @Repository
 public class CompDAOImpl implements CompDAO {
-	
+
 	@Inject
 	private SqlSession sqlSession;
-	
+
 	private static final String namespace="com.itwillbs.mappers.compMapper";
 	@Override
-	public void insertProd(ProdDTO prodDTO) {
+	public void insertProd(ProdDTO prodDTO,Map<String, Object> opMap) {
 		System.out.println("MemberDAOImpl insertProd()");
 		// 마이바티스 메서드 호출
 		sqlSession.insert(namespace + ".insertProd", prodDTO);
-		
-		
+		sqlSession.insert(namespace + ".insertOp", opMap);
+
+
 	}
 //	@Override
 //	public Integer getMaxNum() {
 //		return sqlSession.selectOne(namespace+".getMaxNum");
 //	}
-	
-	
+
+
 }
--- TeamProject/src/main/java/com/itwillbs/service/CompService.java
+++ TeamProject/src/main/java/com/itwillbs/service/CompService.java
@@ -1,10 +1,12 @@
 package com.itwillbs.service;
 
+import java.util.Map;
+
 import com.itwillbs.domain.ProdDTO;
 import com.itwillbs.domain.ProdStockDTO;
 
 public interface CompService {
 
-	void insertProd(ProdDTO prodDTO);
+	void insertProd(ProdDTO prodDTO, Map<String, Object> opMap);
 
 }
