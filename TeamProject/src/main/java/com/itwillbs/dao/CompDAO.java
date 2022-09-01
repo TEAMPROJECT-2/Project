6d5b12896a16d3f7a0024265eba8dbe7ae33b4c1
k_nar <k_nar@DESKTOP-FTMSIG5>
2022-09-01 21:18:03 +0900

    220901 삭제기능 추가(삭제후 페이지 새로고침은 안됨)

--- TeamProject/src/main/java/com/itwillbs/dao/CompDAO.java
+++ TeamProject/src/main/java/com/itwillbs/dao/CompDAO.java
@@ -1,13 +1,23 @@
 package com.itwillbs.dao;
 
+import java.util.List;
 import java.util.Map;
 
+import com.itwillbs.domain.PageDTO;
 import com.itwillbs.domain.ProdDTO;
 import com.itwillbs.domain.ProdStockDTO;
 
 public interface CompDAO {
 
 	public void insertProd(ProdDTO prodDTO, Map<String, Object> opMap);
+
+	ProdDTO getProd(int num);
+
+	List<ProdDTO> getProdList(PageDTO pageDTO);
+
+	int getProdCount();
+
+	public void deleteProd(String num);
 
 
 
