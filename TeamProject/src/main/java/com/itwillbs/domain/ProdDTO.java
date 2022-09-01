a3f7b3801e3ed0b1d719f037c45608f91163ae13
이윤경 <110517088+Yun960115@users.noreply.github.com>
2022-09-01 11:21:02 +0900

    ProdDTO 새로 추가
    2022-09-01
    Product 관련 추가

---
+++ TeamProject/src/main/java/com/itwillbs/domain/ProdDTO.java
@@ -0,0 +1,100 @@
+package com.itwillbs.domain;
+
+import java.sql.Timestamp;
+
+public class ProdDTO extends PageDTO {
+
+	private int prodLNum;
+	private String prodLCode;
+	private String prodLType;
+	private String prodLOption1;
+	private String prodLOption2;
+	private String prodLProdnm;
+	private int prodLPrice;
+	private Timestamp prodLUpdate;
+	private String prodLCompnm;
+	private String prodLMainimg;
+	private String prodLSubimg;
+	private String prodLDetail;
+	private int prodLQuantity;
+
+	public int getProdLNum() {
+		return prodLNum;
+	}
+	public void setProdLNum(int prodLNum) {
+		this.prodLNum = prodLNum;
+	}
+	public String getProdLCode() {
+		return prodLCode;
+	}
+	public void setProdLCode(String prodLCode) {
+		this.prodLCode = prodLCode;
+	}
+	public String getProdLType() {
+		return prodLType;
+	}
+	public void setProdLType(String prodLType) {
+		this.prodLType = prodLType;
+	}
+	public String getProdLOption1() {
+		return prodLOption1;
+	}
+	public void setProdLOption1(String prodLOption1) {
+		this.prodLOption1 = prodLOption1;
+	}
+	public String getProdLOption2() {
+		return prodLOption2;
+	}
+	public void setProdLOption2(String prodLOption2) {
+		this.prodLOption2 = prodLOption2;
+	}
+	public String getProdLProdnm() {
+		return prodLProdnm;
+	}
+	public void setProdLProdnm(String prodLProdnm) {
+		this.prodLProdnm = prodLProdnm;
+	}
+	public int getProdLPrice() {
+		return prodLPrice;
+	}
+	public void setProdLPrice(int prodLPrice) {
+		this.prodLPrice = prodLPrice;
+	}
+	public Timestamp getProdLUpdate() {
+		return prodLUpdate;
+	}
+	public void setProdLUpdate(Timestamp prodLUpdate) {
+		this.prodLUpdate = prodLUpdate;
+	}
+	public String getProdLCompnm() {
+		return prodLCompnm;
+	}
+	public void setProdLCompnm(String prodLCompnm) {
+		this.prodLCompnm = prodLCompnm;
+	}
+	public String getProdLMainimg() {
+		return prodLMainimg;
+	}
+	public void setProdLMainimg(String prodLMainimg) {
+		this.prodLMainimg = prodLMainimg;
+	}
+	public String getProdLSubimg() {
+		return prodLSubimg;
+	}
+	public void setProdLSubimg(String prodLSubimg) {
+		this.prodLSubimg = prodLSubimg;
+	}
+	public String getProdLDetail() {
+		return prodLDetail;
+	}
+	public void setProdLDetail(String prodLDetail) {
+		this.prodLDetail = prodLDetail;
+	}
+	public int getProdLQuantity() {
+		return prodLQuantity;
+	}
+	public void setProdLQuantity(int prodLQuantity) {
+		this.prodLQuantity = prodLQuantity;
+	}
+
+}
